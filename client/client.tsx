6175ea14f4db64cab1aa8696fe4a8f42bf7a103f
DOBBI <cocomong_94@naver.com>
2022-06-09 02:08:37 +0900

    FEAT : LOGIN, LOGOUT

--- client/client.tsx
+++ client/client.tsx
@@ -9,3 +9,17 @@ render(
   </BrowserRouter>,
   document.querySelector('#app'),
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './layouts/App';
+
+// const rootElement = document.getElementById('root');
+// if (!rootElement) throw new Error('Failed to find the root element');
+// const root = ReactDOM.createRoot(rootElement);
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
